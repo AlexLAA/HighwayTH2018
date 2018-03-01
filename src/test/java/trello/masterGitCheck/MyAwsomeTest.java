790244f69eb0dbc564ec61e2a2afec6483573fb0
Alexey Lakovych <lolik@Alexeys-MacBook-Pro.local>
2018-03-01 20:40:06 +0200

    added class

---
+++ src/test/java/trello/masterGitCheck/MyAwsomeTest.java
@@ -0,0 +1,11 @@
+package trello.masterGitCheck;
+
+/**
+ * Created by lolik on 3/1/18.
+ */
+public class MyAwsomeTest {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/test/java/trello/settings/MySettingsTest.java
@@ -0,0 +1,8 @@
+package trello.settings;
+
+/**
+ * Created by lolik on 3/1/18.
+ */
+public class MySettingsTest {
+
+}
