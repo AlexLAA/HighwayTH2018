52f6501f70c527ef6e84561dfc216a16c893fc9b
Alexey Lakovych <lolik@Alexeys-MacBook-Pro.local>
2018-03-02 15:47:42 +0200

    new branch update

---
+++ src/test/java/trello/BranchUpdate.java
@@ -0,0 +1,10 @@
+package trello;
+
+/**
+ * Created by lolik on 3/2/18.
+ */
+public class BranchUpdate {
+    public static void main(String[] args) {
+        System.out.println("asdasd");
+    }
+}
